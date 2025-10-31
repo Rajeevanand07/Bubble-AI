b915d4e2fa01f173958f8a225307e32ffc8e2177
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-11-01 00:56:01 +0530

    changed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,23 +8,19 @@ const path = require('path')
 const app = express()
 
 app.use(cors({
-    origin: "https://bubble-ai-z0vn.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
 app.use(cookieparser());
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api/auth',authRouter)
 app.use('/api/chat',chatRouter)
 
-app.use(express.static(path.join(__dirname, '../public')))
-
-app.get('*', (req, res, next) => {
-    if (req.url.startsWith('/api/')) {
-        return next();
-    }
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 
 module.exports = app
